b8ba1182ca51601f68fa187dcd76fd7bcfc133e3
iamluismoran <iamluismoran@gmail.com>
2025-06-18 17:03:26 +0200

    Solved lab

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 // Iteration 1: Names and Input
+let hacker1 = prompt ("Hey Driver, what is your name?");
+let hacker2 = prompt ("Hey Navigator, what is your name?");
 
+console.log(`The driver's name is ${hacker1}.`);
+console.log(`The navigator's name is ${hacker2}.`);
 
 // Iteration 2: Conditionals
-
+if (hacker1.length > hacker2.length) {console.log(`The driver has the longest name, it has ${hacker1.length} characcters.`)}
+else if (hacker2.length > hacker1.length) {console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`)}
+else {console.log (`Wow, you both have equally long names,${hacker1.length} characters.`)}
 
 // Iteration 3: Loops
